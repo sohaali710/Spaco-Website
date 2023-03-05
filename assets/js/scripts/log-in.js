4b79a5b25df45635db10459a8346927451c5d74c
Soha <sohaali710@gmail.com>
2023-03-05 14:03:37 +0200

    Adding Form Validation

--- assets/js/scripts/log-in.js
+++ assets/js/scripts/log-in.js
@@ -1,34 +1,89 @@
 const formElement = document.getElementById('log-in-form');
 
-let flag = false;
+let emailInput = document.getElementById('email')
+let passwordInput = document.getElementById('password')
+
+let data = {};
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+
 
 formElement.addEventListener('submit', event => {
     event.preventDefault();
 
+    checkEmail()
+    checkPassword()
+
     const formData = new FormData(formElement);
+    data = Object.fromEntries(formData)
 
-    const data = Object.fromEntries(formData)
-    console.log(data)
-
-    if (flag) {
-        fetch('https://reqres.in/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+    fetch('https://reqres.in/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => {
+            setFormError()
+            if (res.status == 200) {
+                console.log(res);
+                location.href = 'index.html';
+                return res.json();
+            }
         })
-            .then(res => {
-                if (res.status == 200) {
-                    console.log(res);
-                    location.href = 'index.html';
-                    return res.json();
-                } else {
-                    alert('user name or password are wrong, try again')
-                }
-            })
-            .then(data => console.log(data))
-            .catch(err => console.log(err))
-    }
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
 
 })
+
+
+let checkEmail = () => {
+    if (emailInput.value === '') {
+        setErrorFor(emailInput, 'من فضلك ادخل عنوان بريدك الإلكتروني.')
+    } else if (emailRegex.test(emailInput.value)) {
+        setErrorFor(emailInput, 'البريد الإلكتروني يجب أن يحتوي على علامة @ و حرفين بعدها على الأقل.')
+    } else {
+        setSuccessFor(emailInput)
+    }
+}
+
+
+let checkPassword = () => {
+    if (passwordInput.value === '') {
+        setErrorFor(passwordInput, 'من فضلك ادخل كلمة المرور.')
+    } else if (passwordRegex.test(passwordInput.value)) {
+        setErrorFor(passwordInput, 'كلمة المرور يجب أن تحتوي على أحرف و أرقام و رموز.')
+    } else {
+        setSuccessFor(passwordInput)
+    }
+}
+
+let setSuccessFor = (input) => {
+    const formControl = input.parentElement
+
+    formControl.className = "custom-form-control success"
+}
+let setErrorFor = (input, msg) => {
+    const formControl = input.parentElement
+    const small = formControl.querySelector('small')
+
+    small.innerText = msg
+
+    formControl.className = "custom-form-control error"
+}
+
+let setFormError = () => {
+    if (emailInput.parentElement.classList.contains('success') && passwordInput.parentElement.classList.contains('success')) {
+        const formErrorMsg = formElement.querySelector('.formErrorMsg')
+        formErrorMsg.innerText = 'هذا البريد الإلكتروني غير موجود. من فضلك ادخل بريد إلكتروني صحيح أو قم بإنشاء حساب جديد.'
+
+        formElement.className = "error"
+
+        emailInput.parentElement.className = "custom-form-control error"
+        passwordInput.parentElement.className = "custom-form-control error"
+        emailInput.parentElement.querySelector('small').innerHTML = ''
+        passwordInput.parentElement.querySelector('small').innerHTML = ''
+
+    }
+}
