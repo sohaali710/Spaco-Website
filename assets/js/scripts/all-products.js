02649c236e4f192f199cc18d050be54d375336d4
Soha <sohaali710@gmail.com>
2023-03-12 21:37:34 +0200

    rendering all products form database

--- assets/js/scripts/all-products.js
+++ assets/js/scripts/all-products.js
@@ -2,37 +2,36 @@ let allProductsDiv = document.querySelector('.all-products')
 let allProducts = []
 
 let moreDetailsBtn = document.getElementById('more-details')
+let url = 'http://localhost:5000/products/all'
 
-
-fetch('http://localhost:5000/products/all')
+fetch(url)
     .then(res => {
         if (res.status == 200) {
-            // console.log(res);
-            // location.href = 'products-CRUD.html';
+            console.log(res);
             return res.json();
         }
     })
     .then(data => {
-        // console.log(data)
+        console.log(data)
         allProducts = data.data
-        console.log(allProducts)
 
 
         allProducts.map((product) => {
+            let { _id, name, category, description, details, imgs } = product
+
             let lis = ''
 
-            for (let i of product.details) {
-                // console.log(i)
-                lis += `<li>${i.title} : ${i.value}</li>`
+            for (let i of details) {
+                lis += `<li><span>${i.title} : ${i.value}</span></li>`
             }
 
             allProductsDiv.innerHTML += `<div class="rental-item">
                 <div class="rental-item__media"> <img src="./assets/img/rental-item-1.jpg" alt="Standard Excavator"></div>
                 <div class="rental-item__desc" dir="rtl">
-                    <div class="rental-item__title">${product.name}</div>
+                    <div class="rental-item__title">${name}</div>
                     <div class="rental-item__price-delivery"> <span>تفاصيل المنتج</span></div>
                     <div class="rental-item__specifications">
-                        <ul class="uk-list uk-list-disc uk-column-1-1@s uk-column-1-2@s">`
+                        <ul class="uk-column-1-1@s uk-column-1-2@s">`
                 + lis +
                 `</ul>
                     </div>
@@ -41,19 +40,13 @@ fetch('http://localhost:5000/products/all')
                         <button class="uk-button uk-button-large uk-width-1-1" type="submit"><span>اضف إلى
                             السلة</span><img src="./assets/img/icons/cart-shopping-solid.svg" alt=""
                             style="margin-right: 10px;"></button>
-                        <a onclick="moreDetails(product._id)" class="uk-button uk-button-large uk-button-secondary"
-                        type="submit" id="more-details"><span>عرض تفاصيل
-                            أكثر</span></a>
+                        <a href="equipment-detail.html?id=${_id}" class="uk-button uk-button-large uk-button-secondary" type="submit" id="more-details">
+                            <span>عرض تفاصيل أكثر</span>
+                        </a>
                     </div>
                 </div>
             </div>`
+
         })
     })
     .catch(err => console.log(err))
-
-
-/**products-details */
-function moreDetails(id) {
-    // console.log(event.target.parentElement.parentElement.parentElement.parentElement)
-    console.log(id)
-}
