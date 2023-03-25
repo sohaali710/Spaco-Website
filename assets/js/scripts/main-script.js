6e89802a3b07305c3fae4d4ebefb6df92f82a55c
Soha <sohaali710@gmail.com>
2023-03-26 01:58:22 +0200

    rendering categories in nav & side nav

--- assets/js/scripts/main-script.js
+++ assets/js/scripts/main-script.js
@@ -35,3 +35,20 @@ cartIcon.addEventListener('click', () => {
         location.href = 'user-supplier-log-in.html?user-type=user'
     }
 })
+
+
+//#region get categories in nav & side nav (in small screens)
+let categoriesUl = document.querySelectorAll('.nav-categories')
+
+let lis = ''
+
+fetch('http://linkloop.co:5000/products/categs').then(res => res.json()).then(data => {
+    data.data.forEach((categ => {
+        lis += `<li><a href="./page-category-products.html?category=${categ.name}">${categ.name}</a></li>`
+        console.log(lis)
+    }))
+
+    categoriesUl.forEach((ul) => {
+        ul.innerHTML = lis
+    })
+})
