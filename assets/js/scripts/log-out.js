3db91ad280c87206fddfa8e3eb2b93ee7fb8877d
Soha <sohaali710@gmail.com>
2023-04-06 03:34:33 +0200

    handle log out for multiple users types

--- assets/js/scripts/log-out.js
+++ assets/js/scripts/log-out.js
@@ -5,20 +5,27 @@ let logOutBtn = document.getElementById('log-out')
 
 let userToken = 'user_access_token'
 let supplierToken = 'supplier_access_token'
+let adminToken = 'admin_access_token'
 
-let redirectTo = 'user-supplier-log-in.html'
+let redirectTo = ''
 
 if (getCookie(userToken)) {
     logInOutNav(userToken)
 } else if (getCookie(supplierToken)) {
     logInOutNav(supplierToken)
+} else if (getCookie(adminToken)) {
+    logInOutNav(adminToken)
 }
 
 logOutBtn.addEventListener('click', event => {
     if (getCookie(userToken)) {
         deleteCookie(userToken)
+        location.href = 'user-supplier-log-in.html'
     } else if (getCookie(supplierToken)) {
         deleteCookie(supplierToken)
+        location.href = 'user-supplier-log-in.html'
+    } else if (getCookie(adminToken)) {
+        deleteCookie(adminToken)
+        location.href = 'admin-log-in.html'
     }
-    location.href = redirectTo
 })
