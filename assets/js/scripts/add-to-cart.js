aee27ae4e4d162a5eaba944979e32a91241242a9
Soha <sohaali710@gmail.com>
2023-04-10 03:17:57 +0200

    handle a little bug

--- assets/js/scripts/add-to-cart.js
+++ assets/js/scripts/add-to-cart.js
@@ -2,8 +2,10 @@ import { setCookie, getCookie } from './cookies.js'
 
 let cartProducts = []
 
-function addToCart(cartIconCount) {
+function addToCart() {
     return (e) => {
+        let cartIconCount = document.querySelector('.cart-btn__icon')
+
         if (e.target.matches("#addQuantity")) {
             let productId = e.target.getAttribute('product-id')
             let productQuantity = e.target.parentElement.querySelector('.quantity-input').value;
@@ -11,27 +13,12 @@ function addToCart(cartIconCount) {
             let count = document.createElement('span')
             count.classList.add('cart-btn__count')
 
-            if (getCookie('user-cart')) {
-                cartProducts = JSON.parse(getCookie('user-cart'))
 
-                let exist = cartProducts.find(p => p.product === productId)
+            cartProducts.push({ product: productId, quantity: productQuantity })
+            setCookie('user-cart', JSON.stringify(cartProducts))
 
-                if (exist) {
-                    alert('product already exists')
-                } else {
-                    cartProducts.push({ product: productId, quantity: productQuantity })
-                    setCookie('user-cart', JSON.stringify(cartProducts))
-                }
-
-                count.textContent = cartProducts.length
-                cartIconCount.append(count);
-            } else {
-                cartProducts.push({ product: productId, quantity: productQuantity })
-                setCookie('user-cart', JSON.stringify(cartProducts))
-
-                count.textContent = '1'
-                cartIconCount.append(count);
-            }
+            count.textContent = cartProducts.length
+            cartIconCount.append(count);
 
             e.target.parentElement.style.display = 'none';
             e.target.parentElement.parentElement.querySelector("#addToCart").style.display = 'inline';
@@ -39,14 +26,27 @@ function addToCart(cartIconCount) {
 
         } else if (e.target.matches("#addToCart")) {
             if (getCookie('user_access_token')) {
-                e.target.style.display = 'none';
-                e.target.parentElement.querySelector(".add-product-quantity").style.display = 'flex';
+                let productId = e.target.getAttribute('product-id')
+
+                if (getCookie('user-cart')) {
+                    cartProducts = JSON.parse(getCookie('user-cart'))
+
+                    let exist = cartProducts.find(p => p.product === productId)
+
+                    if (exist) {
+                        alert('لقد أضفت هذا المنتج من قبل')
+                    } else {
+                        e.target.style.display = 'none';
+                        e.target.parentElement.querySelector(".add-product-quantity").style.display = 'flex';
+                    }
+                } else {
+                    e.target.style.display = 'none';
+                    e.target.parentElement.querySelector(".add-product-quantity").style.display = 'flex';
+                }
             } else {
-                //redirect to login page
                 location.href = 'user-supplier-log-in.html'
             }
         }
     }
 }
-
 export { addToCart }
