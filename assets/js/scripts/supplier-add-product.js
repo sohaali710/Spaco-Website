f89c9dd89a776d8ccc6db5f0d3caf13b96027cbb
Soha <sohaali710@gmail.com>
2023-04-10 03:20:33 +0200

    handle add to store button

--- assets/js/scripts/supplier-add-product.js
+++ assets/js/scripts/supplier-add-product.js
@@ -3,7 +3,7 @@ import { getCookie } from './cookies.js'
 let supplierToken = 'supplier_access_token'
 
 /** from all products page */
-function addToStore(productId) {
+function addToStoreAPI(productId) {
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
     myHeaders.append('authorization', `Bearer ${getCookie(supplierToken)}`);
@@ -27,4 +27,65 @@ function addToStore(productId) {
         .catch(err => console.log(err))
 }
 
+
+function addToStore() {
+    return (e) => {
+        if (e.target.matches("#addToStore")) {
+            let productId = e.target.getAttribute('product-id')
+
+            const myHeaders = new Headers();
+            myHeaders.append('authorization', `Bearer ${getCookie(supplierToken)}`);
+
+            const options = {
+                method: 'GET',
+                headers: myHeaders
+            }
+
+            fetch('http://linkloop.co:5000/supplier/all-products', options)
+                .then(res => { if (res.status == 200) return res.json() })
+                .then(data => {
+                    if (data) {
+                        const supplierProducts = data.products
+
+                        if (supplierProducts.length !== 0) {
+                            console.log(supplierProducts)
+                            supplierProducts.forEach((product) => {
+                                if (product._id === productId) {
+                                    alert('لقد أضفت هذا المنتج إلى متجرك من قبل.')
+                                } else {
+                                    addToStoreAPI(productId)
+
+                                    let addToStoreBtn = e.target
+                                    let addedBtn = e.target.parentElement.children[1]
+
+                                    addedBtn.style.display = 'inline'
+                                    addToStoreBtn.style.display = 'none'
+                                    setTimeout(() => {
+                                        addedBtn.style.display = 'none'
+                                        addToStoreBtn.style.display = 'inline'
+                                    }, 1500);
+                                }
+                            })
+                        } else {
+                            addToStoreAPI(productId)
+
+                            let addToStoreBtn = e.target
+                            let addedBtn = e.target.parentElement.children[1]
+
+                            addedBtn.style.display = 'inline'
+                            addToStoreBtn.style.display = 'none'
+                            setTimeout(() => {
+                                addedBtn.style.display = 'none'
+                                addToStoreBtn.style.display = 'inline'
+                            }, 1500);
+                        }
+                    }
+                })
+
+        }
+        if (e.target.matches("#added")) {
+            alert('لقد أضفت هذا المنتج إلى متجرك من قبل.')
+        }
+    }
+}
 export { addToStore }
